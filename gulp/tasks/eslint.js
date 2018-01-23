5f6f9b5d1e550f89682d351a0d78503a662ebda1
Richard Deslauriers <richard2richard@gmail.com>
2018-01-22 18:18:47 -0600

    Add eslint task

---
+++ gulp/tasks/eslint.js
@@ -0,0 +1,21 @@
+// -------------------------------------
+//   Task: eslint
+// -------------------------------------
+//
+// - ESLint JS files
+//
+// -------------------------------------
+
+import gulp from 'gulp';
+import eslint from 'gulp-eslint';
+import globs from '../globs';
+
+const eslintTask = () => {
+    return gulp.src([globs.to.js])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+};
+eslintTask.description = 'ESLint JS files';
+
+export default eslintTask;
