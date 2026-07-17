be3b1716746fa48f5e7b6b8ad565486d24945b3c
Richard Deslauriers <richard.deslauriers@slalom.com>
2026-07-17 11:09:12 -0500

    security: replace gulp-htmlmin with html-minifier-terser (#81)
    Closes #80
    
    - html.js + nunjucks.js: swap gulp-htmlmin for html-minifier-terser async minify()
    - package.json.tpl: gulp-htmlmin ^5.0.1 → html-minifier-terser ^7.2.0
    - Result: 0 critical, 0 high vulnerabilities in generated web project
    - 114/114 tests pass

--- templates/base/gulp/tasks/html.js
+++ templates/base/gulp/tasks/html.js
@@ -11,31 +11,44 @@
 
 import browserSync from 'browser-sync';
 import gulp from 'gulp';
-import htmlmin from 'gulp-htmlmin';
+import { minify } from 'html-minifier-terser';
 import plumber from 'gulp-plumber';
 import special from 'gulp-special-html';
-import through from 'through2';
+import through2 from 'through2';
 import commandLineArguments from '../commandLineArguments.js';
 import errorHandler from '../errorHandler.js';
 import globs from '../globs.js';
 
-const htmlTask = () => {
-  return gulp
+const HTMLMIN_OPTIONS = {
+  collapseWhitespace: true,
+  removeComments: true,
+  removeCommentsFromCDATA: true,
+  minifyJS: true,
+};
+
+const minifyHtml = () =>
+  through2.obj((file, _, cb) => {
+    if (file.isNull()) { cb(null, file); return; }
+    (async () => {
+      try {
+        const result = await minify(file.contents.toString('utf-8'), HTMLMIN_OPTIONS);
+        file.contents = Buffer.from(result, 'utf-8');
+      } catch (_err) { /* pass through on minify error */ }
+      cb(null, file);
+    })();
+  });
+
+const htmlTask = () =>
+  gulp
     .src(globs.to.html, { base: globs.to.src })
     .pipe(plumber(errorHandler))
     .pipe(special())
-    .pipe(commandLineArguments.nomin
-      ? through.obj()
-      : htmlmin({
-        collapseWhitespace: true,
-        removeComments: true,
-        removeCommentsFromCDATA: true,
-        minifyJS: true
-      }))
+    .pipe(commandLineArguments.nomin ? through2.obj() : minifyHtml())
     .pipe(plumber.stop())
     .pipe(gulp.dest(globs.to.dist))
     .pipe(browserSync.stream({ once: true }));
-};
+
 htmlTask.description = 'optimize html and move to /dist/';
 
 export default htmlTask;
+
--- templates/base/gulp/tasks/nunjucks.js
+++ templates/base/gulp/tasks/nunjucks.js
@@ -13,17 +13,36 @@
 import browserSync from 'browser-sync';
 import gulp from 'gulp';
 import { nunjucksCompile } from 'gulp-nunjucks';
-import htmlmin from 'gulp-htmlmin';
+import { minify } from 'html-minifier-terser';
 import { marked } from 'marked';
 import markdown from 'nunjucks-markdown';
 import nunjucks from 'nunjucks';
 import plumber from 'gulp-plumber';
 import rename from 'gulp-rename';
 import special from 'gulp-special-html';
-import through from 'through2';
+import through2 from 'through2';
 import commandLineArguments from '../commandLineArguments.js';
 import errorHandler from '../errorHandler.js';
 import globs from '../globs.js';
+
+const HTMLMIN_OPTIONS = {
+  collapseWhitespace: true,
+  removeComments: true,
+  removeCommentsFromCDATA: true,
+  minifyJS: true,
+};
+
+const minifyHtml = () =>
+  through2.obj((file, _, cb) => {
+    if (file.isNull()) { cb(null, file); return; }
+    (async () => {
+      try {
+        const result = await minify(file.contents.toString('utf-8'), HTMLMIN_OPTIONS);
+        file.contents = Buffer.from(result, 'utf-8');
+      } catch (_err) { /* pass through on minify error */ }
+      cb(null, file);
+    })();
+  });
 
 const nunjucksTask = () => {
   const env = new nunjucks.Environment(new nunjucks.FileSystemLoader( globs.to.environment ));
@@ -47,14 +66,7 @@ const nunjucksTask = () => {
     .pipe(rename((file) => {
       file.extname = '.html';
     }))
-    .pipe(commandLineArguments.nomin
-      ? through.obj()
-      : htmlmin({
-        collapseWhitespace: true,
-        removeComments: true,
-        removeCommentsFromCDATA: true,
-        minifyJS: true
-      }))
+    .pipe(commandLineArguments.nomin ? through2.obj() : minifyHtml())
     .pipe(plumber.stop())
     .pipe(gulp.dest(globs.to.dist))
     .pipe(browserSync.stream({ once: true }));
